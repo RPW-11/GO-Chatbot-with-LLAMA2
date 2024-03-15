1c0485661cb762004f0607267ad7a7a5db55819a
Rainata Putra <kriegcom77@gmail.com>
2024-03-15 23:30:00 +0700

    load data function

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GO-Chatbot-with-LLAMA2
 
 ### To start the app, run the following code
-```
+```bash
 bash setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CMAKE_ARGS="-DLLAMA_METAL=on" FORCE_CMAKE=1 pip install --upgrade --force-reinstall llama-cpp-python --no-cache-dir
+!CMAKE_ARGS="-DLLAMA_METAL=on" FORCE_CMAKE=1 pip install llama-cpp-python
 pip install -r requirements.txt
